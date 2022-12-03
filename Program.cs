7e3012bd93ce1168c1a67d9c1dfacac08cac374c
e.kitsman <e.kitsman@yandex.ru>
2022-12-03 18:36:25 +0300

    Added task 1 and 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашняя работа 1.
+// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write(" Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Input a second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1>num2)
+// {   Console.WriteLine($"max={num1} and min={num2}");
+// }
+// else
+// {
+//    Console.WriteLine($"min={num1} and max={num2}");
+// }
+
+// Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Input an integer number N:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = 2;
+
+// while (current <= num)
+// {
+//     Console.Write (current + " ");
+//     current+=2;
+// }
