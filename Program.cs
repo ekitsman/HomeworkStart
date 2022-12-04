3dd132ad5bc1f80e4ba2ebfd398d99008c64b5f5
e.kitsman <e.kitsman@yandex.ru>
2022-12-04 15:29:32 +0300

    Added task 2 and 3

--- Program.cs
+++ Program.cs
@@ -14,6 +14,43 @@
 //    Console.WriteLine($"min={num1} and max={num2}");
 // }
 
+
+
+// Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write(" Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Input a second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Input the third number:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2>num1) max = num2;
+// if (num3>num2) max = num3;
+
+// Console.WriteLine($"MAX={max}");
+
+
+
+// Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Input a number :");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0) 
+// {
+//     Console.WriteLine(" Число является четным");
+// }
+
+// else 
+// {
+//     Console.WriteLine(" Число является нечетным");
+// }
+
+
+
 // Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // Console.Write("Input an integer number N:");
